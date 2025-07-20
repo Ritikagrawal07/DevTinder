9b076bbdd9e12abe73324b229ef1ed1ad7a28db5
https://github.com/Ritikagrawal07 <ritikkumar56678@gmail.com>
2025-07-20 10:27:50 +0530

    adding gitignore file

--- src/app.js
+++ src/app.js
@@ -6,9 +6,4 @@ app.use("/test",(req,res) => {
 })
 app.use("/",(req,res) => {
     res.send("Hello from server");
-})
-
-app.listen((3000),() => {
-    console.log('Server is running on port 3000');
-})
-
+}) 
